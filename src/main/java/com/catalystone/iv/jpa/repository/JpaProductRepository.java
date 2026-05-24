43c816b6dadf928a2b143606cb21c5d51816a3af
djinn909 <>
2026-05-24 18:01:02 +0200

    Refactor JpaProductRepository to implement correct SQL queries for name and ID retrieval

--- src/main/java/com/catalystone/iv/jpa/repository/JpaProductRepository.java
+++ src/main/java/com/catalystone/iv/jpa/repository/JpaProductRepository.java
@@ -10,13 +10,18 @@ import com.catalystone.iv.jpa.model.JpaProduct;
 
 public interface JpaProductRepository extends JpaRepository<JpaProduct, Long> {
 
-    @Query(value = "write your query", nativeQuery = true)
+    @Query(value = "SELECT name FROM PRODUCT WHERE id = :productId", nativeQuery = true)
     String nameById(Long productId);
 
-    @Query(value = "write your query", nativeQuery = true)
+    @Query(value = "SELECT id FROM PRODUCT WHERE name = :productName", nativeQuery = true)
     Long idByName(String productName);
 
-    @Query(value = "write your query", nativeQuery = true)
+    @Query(value = """
+        SELECT id, name, price, category
+        FROM PRODUCT
+        ORDER BY price DESC
+        LIMIT 1 OFFSET :#{#parameter - 1}
+        """, nativeQuery = true)
     JpaProduct productWithXHighestPrice(int parameter);
 
     @Query("""
@@ -26,7 +31,11 @@ public interface JpaProductRepository extends JpaRepository<JpaProduct, Long> {
         """)
     List<CategoryCount> categoryCount();
 
-    @Query(value = "write your query", nativeQuery = true)
+    @Query(value = """
+        SELECT id
+        FROM PRODUCT
+        ORDER BY name ASC, price DESC
+        """, nativeQuery = true)
     List<Long> allIdsAlphabeticalOrder();
 
     List<JpaProduct> getProductsByName(String name);
